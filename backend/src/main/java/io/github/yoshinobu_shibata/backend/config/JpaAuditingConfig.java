ea15f7beef8b1c5e9b923aa341220ac276199413
YOSHINOBU_S\webyo <web.yoshinobu@gmail.com>
2026-08-25 13:43:20 +0900

    Entity共通部分関連ファイルにコメントを追加

--- backend/src/main/java/io/github/yoshinobu_shibata/backend/config/JpaAuditingConfig.java
+++ backend/src/main/java/io/github/yoshinobu_shibata/backend/config/JpaAuditingConfig.java
@@ -5,5 +5,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
+/**
+ * JPA監査機能 有効化コンフィグクラス
+ *
+ * <p>
+ *  @EnableJpaAuditingはアノテーションだけで機能が有効化される仕組みのため
+ *  、フィールドやメソッドは不要
+ * </p>
+ *
+ * @author  Yoshinobu Shibata
+ * @version 1.0
+ * @since   2026-08-25
+ */
 public class JpaAuditingConfig {
 }
--- backend/src/main/java/io/github/yoshinobu_shibata/backend/entity/BaseEntity.java
+++ backend/src/main/java/io/github/yoshinobu_shibata/backend/entity/BaseEntity.java
@@ -14,19 +14,30 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+/**
+ * 全Entityが継承する共通親クラス
+ *
+ * @author  Yoshinobu Shibata
+ * @version 1.0
+ * @since   2026-08-25
+ */
 public abstract class BaseEntity {
 
+    // レコード作成日時
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    // レコード作成元API
+    @Column(name = "created_by_source", updatable = false)
+    private String createdBySource;
+
+    // レコード最終更新日時
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @Column(name = "created_by_source", updatable = false)
-    private String createdBySource;
-
+    // レコード更新元API
     @Column(name = "updated_by_source")
     private String updatedBySource;
 }
